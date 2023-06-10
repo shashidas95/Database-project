be7a64d218edad8d61927ce2b71e8b87ff37b029
shashidas95 <shashidas95@gmail.com>
2023-06-10 15:00:28 +0600

    c

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Database\Query\JoinClause;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
@@ -10,7 +11,10 @@ class DemoController extends Controller
     function DemoAction()
     {
         $result = DB::table('products')
-            ->join('categories', 'products.category_id', '=', 'categories.id')
+            ->join('categories', function(JoinClause $join){
+
+            $join->on('products.category_id', '=', 'categories.id');
+            }
             ->join('brands', 'products.brand_id', '=', 'brands.id')
             ->get();
         return $result;
