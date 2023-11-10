baae39d345f4d1a91cd317cb41d31477b6dad79e
shashidas95 <shashidas95@gmail.com>
2023-11-10 13:41:58 +0600

    demo  2

--- app/Http/Controllers/DemoController.php
+++ app/Http/Controllers/DemoController.php
@@ -14,6 +14,8 @@ class DemoController extends Controller
             ->join('categories', function (JoinClause $join) {
                 $join->on('products.category_id', '=', 'categories.id')
                     ->where('products.price', '>', 2000);
+            })->join('brands', function(JoinClause $join){
+                $join->on('products.brand_id', '=', 'brands.id')->where('brands.brandName', '=', 'Easy');
             })
             ->get();;
 
